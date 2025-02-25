57b8555f348dfa25aafc862a5e5feebf3d63e9d3
w1zzball <aaronibbo@gmail.com>
2025-02-25 20:37:24 +0000

    add excerpt and updated_on fields to Post model; create Comment model with related fields

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,15 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.CharField(max_length=200, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment_author')
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
