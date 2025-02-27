a5e6c599684847ec8b64bcee2477708686f56bb9
w1zzball <aaronibbo@gmail.com>
2025-02-27 12:19:29 +0000

    Refactor PostList view: update template to index.html, add pagination, and remove old post_list.html

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,7 +66,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
